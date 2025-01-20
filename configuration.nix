195c368a4bb70e188420268b912f52c9ecf99754
jawor182 <kubajaworski59@outlook.com>
2025-01-20 18:12:43 +0100

    updates

--- configuration.nix
+++ configuration.nix
@@ -23,6 +23,10 @@
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  hardware.logitech.wireless = {
+    enable = true;
+    enableGraphical = true;
+  };
 
   # Enable networking
   networking.networkmanager.enable = true;
